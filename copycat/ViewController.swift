4fa5ce54712f1a24da6863e761f76087bf4d372f
tnydg99 <austin.c.tucker14@gmail.com>
2017-06-02 11:35:44 -0400

    Initial Commit

---
+++ copycat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  copycat
+//
+//  Created by Austin Tucker on 6/2/17.
+//  Copyright © 2017 Austin Tucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

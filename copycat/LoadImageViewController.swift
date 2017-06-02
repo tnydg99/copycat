ab43f3beffd4021c4162c50df0b75d8f3548c2c2
tnydg99 <austin.c.tucker14@gmail.com>
2017-06-02 14:08:18 -0400

    Initial launch of copycat with view controller files.

---
+++ copycat/LoadImageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoadImageViewController.swift
+//  copycat
+//
+//  Created by Austin Tucker on 6/2/17.
+//  Copyright © 2017 Austin Tucker. All rights reserved.
+//
+
+import UIKit
+
+class LoadImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

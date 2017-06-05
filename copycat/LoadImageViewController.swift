9b499a49b58cc97da6fb850a9741dc91cc5ce10c
tnydg99 <austin.c.tucker14@gmail.com>
2017-06-05 14:42:09 -0400

    adding new view controller to handle image picking.

--- copycat/LoadImageViewController.swift
+++ copycat/LoadImageViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import SafariServices
+import Photos
 
-class LoadImageViewController: UIViewController, SFSafariViewControllerDelegate {
+class LoadImageViewController: UIViewController, SFSafariViewControllerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +23,35 @@ class LoadImageViewController: UIViewController, SFSafariViewControllerDelegate 
     }
     
     @IBAction func loadImageButtonPressed(_ sender: UIButton) {
-        guard let url = URL(string: "https://www.google.com") else {
+        guard let url = URL(string: "https://images.google.com/") else {
             return
         }
+        let imageAlert = UIAlertController(title: "Load Image", message: "Please select a location to load images from.", preferredStyle: .actionSheet)
+        let safariAction = UIAlertAction(title: "Safari", style: .default, handler: {
+        _ in
+            let safari = SFSafariViewController(url: url)
+            safari.delegate = self
+            self.present(safari, animated: true, completion: nil)
+        })
+        let photosAction = UIAlertAction(title: "Photos", style: .default, handler: {
+        _ in
+            guard let imagePicker = self.storyboard?.instantiateViewController(withIdentifier: "UIImagePickerController") as? UIImagePickerController else {
+                return
+            }
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.delegate = self
+            if let mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary) {
+                imagePicker.mediaTypes = mediaTypes
+                self.present(imagePicker, animated: true, completion: nil)
+            } else {
+                print("error loading photo library.")
+            }
+        })
         
-        let safari = SFSafariViewController(url: url)
-        safari.delegate = self
-        present(safari, animated: true, completion: nil)
+        imageAlert.addAction(safariAction)
+        imageAlert.addAction(photosAction)
+        present(imageAlert, animated: true, completion: nil)
     }
 
     /*
@@ -41,12 +64,23 @@ class LoadImageViewController: UIViewController, SFSafariViewControllerDelegate 
     }
     */
     
+    //MARK SFSafariViewControllerDelegate
+    
 //    func safariViewController(_ controller: SFSafariViewController, activityItemsFor URL: URL, title: String?) -> [UIActivity] {
 //        
 //    }
     
     func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
         performSegue(withIdentifier: "toCopycat", sender: nil)
+    }
+    
+    //MARK ImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        performSegue(withIdentifier: "toCopycat", sender: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
     }
 
 }
